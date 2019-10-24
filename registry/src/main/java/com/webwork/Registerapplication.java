f1812b286d53d65159b9e455f54d798683882877
versionwen <49876178+versionwen@users.noreply.github.com>
2019-10-24 19:47:29 +0800

    建立基本maven框架

--- registry/src/main/java/com/webwork/Registerapplication.java
+++ registry/src/main/java/com/webwork/Registerapplication.java
@@ -7,12 +7,12 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
  * @author version
  * @version 1.0
- * @date 2019/10/23 22:59
+ * @date 2019/10/24 19:36
  */
 @SpringBootApplication
 @EnableEurekaServer
-public class RegistryApplication {
+public class Registerapplication {
     public static void main(String[] args) {
-        SpringApplication.run(RegistryApplication.class,args);
+        SpringApplication.run(Registerapplication.class,args);
     }
 }
