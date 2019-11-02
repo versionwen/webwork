8ff47459093ff694db7089104ebd106e126485fd
versionwen <49876178+versionwen@users.noreply.github.com>
2019-11-02 17:41:10 +0800

    增加account，Order模块

---
+++ Apigetway/src/main/java/com/webwork/Apigetway.java
@@ -0,0 +1,15 @@
+package com.webwork;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+@SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
+@EnableDiscoveryClient
+public class Apigetway {
+    public static void main(String[] args) {
+        SpringApplication.run(Apigetway.class,args);
+    }
+}
